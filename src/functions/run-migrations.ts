021b4f7452ae9dcc7b5c7c81d49856517c828b95
cb-vigneshj <92290437+cb-vigneshj@users.noreply.github.com>
2022-11-16 02:45:11 +0400

    db migrations

--- src/functions/run-migrations.ts
+++ src/functions/run-migrations.ts
@@ -1,7 +1,22 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { ALL } from 'dns';
+import { DataSource } from 'typeorm';
 
 export const execute = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log("****  twitter clone ****");
+  new DataSource({
+    type: "postgres",
+    host: process.env.databaseHost,
+    port: 5432,
+    username: process.env.databaseUser,
+    password: process.env.databasePassword,
+    database: process.env.databaseName,
+    synchronize: true,
+    logging: false,
+    entities: ["../entity/**"],
+    migrations: [],
+    subscribers: [],
+}).runMigrations({transaction:'all'})
   return {
     statusCode: 200,
     body: JSON.stringify(
