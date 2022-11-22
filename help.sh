306cd66c82858ad3f0afd08f2f12f01271f06d03
cb-vigneshj <92290437+cb-vigneshj@users.noreply.github.com>
2022-11-22 14:18:02 +0530

    stack output

--- help.sh
+++ help.sh
@@ -1,3 +1,5 @@
+aws cloudformation validate-template --template-body file:///Users/cbit011032/Documents/dynamofl/twitter-minimal-clone/stack.yml
+
 npm run build
 
 npx typeorm-ts-node-esm migration:generate ./src/migrations/create-base-tables -d ./src/data-source.ts
@@ -5,3 +7,4 @@ npx typeorm-ts-node-esm migration:generate ./src/migrations/create-base-tables -
 npx typeorm-ts-node-esm migration:run -d ./src/data-source.ts
 
 npx ts-node dist
+
